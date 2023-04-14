cadb129c264fc134bf2105657e53b69870ab3d5f
mattyx97 <mattia.guarigliq@gmail.com>
2023-04-14 15:42:26 +0200

    Add product commit

---
+++ EddyVogueWeb/src/eddy/vogue/control/AddProductControl.java
@@ -0,0 +1,76 @@
+package eddy.vogue.control;
+
+import java.io.IOException; 
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import eddy.vogue.model.*;
+
+/**
+ * Servlet implementation class ProductControl
+ */
+public class AddProductControl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	// ProductModelDS usa il DataSource
+	// ProductModelDM usa il DriverManager	
+	static boolean isDataSource = true;
+	
+	static ProductModel model;
+	
+	static {
+		if (isDataSource) {
+			model = new ProductModelDS();
+		} else {
+			model = new ProductModelDM();
+		}
+	}
+	
+	public AddProductControl() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+		String action = request.getParameter("action");
+
+		try {
+			if (action != null) {
+			 if (action.equalsIgnoreCase("insert")) {
+					String name = request.getParameter("name");
+					String description = request.getParameter("description");
+					int price = Integer.parseInt(request.getParameter("price"));
+					int quantity = Integer.parseInt(request.getParameter("quantity"));
+
+					ProductBean bean = new ProductBean();
+					bean.setName(name);
+					bean.setDescription(description);
+					bean.setPrice(price);
+					bean.setQuantity(quantity);
+					model.doSave(bean);
+				}
+			}			
+		} catch (SQLException e) {
+			System.out.println("Error:" + e.getMessage());
+		}
+
+	
+		
+
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/AddProduct.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
