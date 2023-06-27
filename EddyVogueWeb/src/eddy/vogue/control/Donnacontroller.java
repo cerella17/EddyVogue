4f7a2af10160081adfe3bab4a9ec9dd7888940dc
cerella17 <91498144+cerella17@users.noreply.github.com>
2023-06-27 20:25:57 +0200

    Update Donnacontroller.java

--- EddyVogueWeb/src/eddy/vogue/control/Donnacontroller.java
+++ EddyVogueWeb/src/eddy/vogue/control/Donnacontroller.java
@@ -72,7 +72,6 @@ public class Donnacontroller extends HttpServlet {
 				}
 			}			
 		} catch (SQLException e) {
-			System.out.println("Error:" + e.getMessage());
 		}
 
 		request.getSession().setAttribute("cart", cart);
@@ -85,7 +84,6 @@ public class Donnacontroller extends HttpServlet {
 			request.removeAttribute("products");
 			request.setAttribute("products", model.doRetrieveWMan(sort));
 		} catch (SQLException e) {
-			System.out.println("Error:" + e.getMessage());
 		}
 
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/donna.jsp");
