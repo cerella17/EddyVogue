381f9d3f48bae7e3d86ff4cb08776311b46113d8
cerella17 <91498144+cerella17@users.noreply.github.com>
2023-06-27 20:53:02 +0200

    Update ProductControl.java

--- EddyVogueWeb/src/eddy/vogue/control/ProductControl.java
+++ EddyVogueWeb/src/eddy/vogue/control/ProductControl.java
@@ -71,8 +71,8 @@ public class ProductControl extends HttpServlet {
 					model.doSave(bean);
 				}
 			}			
-		} catch (SQLException e) {
-		}
+		} catch (SQLException e) 
+		
 
 		request.getSession().setAttribute("cart", cart);
 		request.setAttribute("cart", cart);
@@ -83,8 +83,8 @@ public class ProductControl extends HttpServlet {
 		try {
 			request.removeAttribute("products");
 			request.setAttribute("products", model.doRetrieveAll(sort));
-		} catch (SQLException e) {
-		}
+		} catch (SQLException e) 
+		
 
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/ProductView.jsp");
 		dispatcher.forward(request, response);
