42709b1e36a1e3f22947794a0ab0f94c006f27ce
cerella17 <91498144+cerella17@users.noreply.github.com>
2023-07-03 01:36:59 +0200

    Update CheckoutController.java

--- EddyVogueWeb/src/eddy/vogue/control/CheckoutController.java
+++ EddyVogueWeb/src/eddy/vogue/control/CheckoutController.java
@@ -22,6 +22,7 @@ public class CheckoutController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//non necessario
 		
 	}
 
