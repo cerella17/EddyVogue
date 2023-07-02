8f1de655752e0cb2a3c392566f260e321e53c122
cerella17 <91498144+cerella17@users.noreply.github.com>
2023-07-03 01:20:08 +0200

    Update emailAjax.js

--- EddyVogueWeb/WebContent/script/emailAjax.js
+++ EddyVogueWeb/WebContent/script/emailAjax.js
@@ -1,15 +1,15 @@
 
 $(document).ready(function(){
 	$('#email').change(function(){
-		console.log("change")
 		
-		var email = $('#email').val();
+		
+		let email = $('#email').val();
 		$.ajax({
 			type: 'POST',
 			data:{email:email},
 			url: 'AjaxEmailController',
 			success: function(result){
-console.log(result)				
+				
 				$('#result').text(result.success)
 
 			}
